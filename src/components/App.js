3e803a5c1d8dfb0008bf0e7f0d62a6a641f58957
Adrian Ramos <ramosadrian56@gmail.com>
2020-05-12 07:05:30 -0700

    code re-use with custom HOOKS

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,17 @@
 import React, { useState } from 'react';
-
-import ResoourceList from './ResoourceList';
+import ResourceList from './ResourceList';
+import UserList from './UserList';
 
 const App = () => {
     const [resource, setResource] = useState('posts');
     return (
         <div>
+            <UserList /> 
             <div>
                 <button onClick={() => setResource('posts')}>Posts</button>
                 <button onClick={() => setResource('todos')}>Todos</button>
             </div>
-            <ResoourceList resource={resource}/>
+            <ResourceList resource={resource}/>
         </div>
     );
 };
